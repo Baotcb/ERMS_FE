662ae902699be2842a4c23ed79236a248f424790
Sunshower1260 <155897796+Sunshower1260@users.noreply.github.com>
2026-02-04 13:50:24 +0700

    fix: cập nhập cho khớp với cấu trúc dự án

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -4,3 +4,7 @@
 export { LoadingSpinner } from './loading-spinner'
 export { Alert } from './alert'
 export { BrandLogo } from './brand-logo'
+export { ImageUpload } from './image-upload'
+export { ErrorDialog } from './error-dialog'
+export { default as ErrorBoundary } from './error-boundary'
+export { Skeleton } from './skeleton'
